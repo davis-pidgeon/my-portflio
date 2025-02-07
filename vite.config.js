54dcdf7782a8d2d7b29412dd9579f7cdf9d5966b
davis-pidgeon <davispidgeon81@gmail.com>
2025-02-07 13:12:20 -0500

    Fix Vite Build output directory

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/",
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
 });
