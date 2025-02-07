0237efa3c11ab29bf406635c4071fd3d25200136
davis-pidgeon <davispidgeon81@gmail.com>
2025-02-07 14:23:39 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import Portfolio from "./App";
+import "./index.css"; // Ensure styles are imported
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Portfolio />
   </React.StrictMode>
 );
