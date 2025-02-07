04e4d4026114838dc6b9860bb9fe8c3c8d8f9696
davis-pidgeon <davispidgeon81@gmail.com>
2025-02-07 12:36:10 -0500

    Added React project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div>
+        <h1>Welcome to My Portfolio</h1>
+        <p>This is my React-based portfolio.</p>
+      </div>
+    );
+  }
+  
+  export default App;
+  
