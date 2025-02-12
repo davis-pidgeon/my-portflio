9756902bf602b041b23c92ca0453ee3b05c010c2
davis-pidgeon <davispidgeon81@gmail.com>
2025-02-11 23:26:07 -0500

    Added Skills, Contact Me, & Updated Case Studies

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ useEffect(() => {
 
   const imageSize = isMobile ? "w-[300px] h-[200px]" : "w-[500px] h-[300px]";
 
+  const tagStyles = "bg-[#5A5A5A] text-white px-4 py-2 rounded-lg text-sm font-medium shadow-md text-center flex items-center justify-center";
+
     // Track hovered images using useRef
   const hoveredPhotosRef = useState(new Set());
 
@@ -137,26 +139,83 @@ useEffect(() => {
         {/* Case Studies & Helpful Links Section */}
         <div className="mt-12">
           <h2 className="text-2xl font-semibold text-center mb-6 text-[#568EA3]">Case Studies & Helpful Links</h2>
-          <div className="flex flex-wrap justify-center gap-8">
-             {[
-               { name: "Thistle", img: "https://davis-pidgeon.github.io/my-portfolio/images/thistle.jpeg", url: "https://www.porterlogic.com/case-studies/porterlogic-helps-thistle-increase-yield-and-improve-margins" },
-              { name: "Hungryroot", img: "https://davis-pidgeon.github.io/my-portfolio/images/hungryroot.jpeg", url: "https://www.porterlogic.com/case-studies/how-hungryroot-got-its-time-back-by-automating-replenishment-with-porterlogic" },
-              { name: "Award", img: "https://davis-pidgeon.github.io/my-portfolio/images/award1.png", url: "https://www.porterlogic.com/news-room/porterlogic-receives-supply-demand-chain-executive-and-food-logistics-2024-top-supply-chain-projects-award" },
-              { name: "LinkedIn", img: "https://davis-pidgeon.github.io/my-portfolio/images/linkedin1.png", url: "https://www.linkedin.com/in/davis-pidgeon-79761a114/" },
-            ].map((client) => (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            {[{
+              name: "Warehouse Optimization",
+              description: "Developed a WMS system that identified over $500K in annual savings by optimizing warehouse operations. Click to learn more!",
+              img: "https://davis-pidgeon.github.io/my-portfolio/images/thistle.jpeg",
+              url: "https://www.porterlogic.com/case-studies/porterlogic-helps-thistle-increase-yield-and-improve-margins"
+            }, {
+              name: "Automated Procurement",
+              description: "Implemented an MRP solution that reduced inventory waste and reclaimed 20% of weekly labor hours. Click to learn more!",
+              img: "https://davis-pidgeon.github.io/my-portfolio/images/hungryroot.jpeg",
+              url: "https://www.porterlogic.com/case-studies/how-hungryroot-got-its-time-back-by-automating-replenishment-with-porterlogic"
+            }, {
+              name: "Award-Winning Project",
+              description: "Recognized by Supply & Demand Chain Executive for innovation in warehouse technology solutions. Click to learn more!",
+              img: "https://davis-pidgeon.github.io/my-portfolio/images/award1.png",
+              url: "https://www.porterlogic.com/news-room/porterlogic-receives-supply-demand-chain-executive-and-food-logistics-2024-top-supply-chain-projects-award"
+            }, {
+              name: "LinkedIn Profile",
+              description: "Let’s connect!",
+              img: "https://davis-pidgeon.github.io/my-portfolio/images/linkedin1.png",
+              url: "https://www.linkedin.com/in/davis-pidgeon-79761a114/"
+            }].map((client) => (
               <motion.a
                 key={client.name}
                 href={client.url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="p-4 bg-gray-800 rounded-lg shadow-md hover:shadow-xl transition-transform transform hover:scale-110"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
+                className="relative p-3 bg-gray-800 rounded-lg shadow-md hover:shadow-xl transition-transform text-white flex flex-col items-center text-center overflow-hidden group"
+                whileHover={{ scale: 1.05 }}
               >
-                <img src={client.img} alt={client.name} className="w-32 h-auto rounded-md" />
+                <img src={client.img} alt={client.name} className="w-56 h-auto rounded-md mb-3" />
+                <h3 className="text-lg font-semibold text-white">{client.name}</h3>
+                <motion.div
+                  className={`bg-[#355F72] text-white text-sm p-2 rounded mt-2 transition-all duration-300 ease-in-out ${isMobile ? 'opacity-100 max-h-40' : 'opacity-0 max-h-0 overflow-hidden group-hover:opacity-100 group-hover:max-h-40'}`}
+                  >
+                  {client.description}
+                </motion.div>
               </motion.a>
             ))}
           </div>
+        </div>
+
+         {/* Functional Experience Section */}
+        <div className="mt-16">
+          <h2 className="text-2xl font-semibold text-center mb-6 text-[#568EA3]">Functional Experience</h2>
+          <div className="flex flex-wrap justify-center gap-3">
+            {["Design", "Configurations", "Wiring Mod Scripts", "SQL & Database Management", "Extension Development & Implementation", "Creating Test Scripts", "Testing Extensions", "Go Live Testing & Support", "Issue Troubleshooting & Resolution", "Performing Code Changes", "Writing SQL Views and Editing Stored Procedures", "Translating Client Needs to Internal Development Teams", "Solution Demos", "Client Meetings & Relationship Management", "Mentoring Junior Consultants", "Delivering SQL Training Classes"].map((item) => (
+              <div key={item} className={tagStyles}>{item}</div>
+            ))}
+          </div>
+        </div>
+
+        {/* Software Proficiency Section */}
+        <div className="mt-16">
+          <h2 className="text-2xl font-semibold text-center mb-6 text-[#568EA3]">Software Proficiency</h2>
+          <div className="flex flex-wrap justify-center gap-3">
+            {["Manhattan WMoS", "PorterLogic", "SQL & Database Management", "VS Code", "Oracle", "Postgres", "Postman", "ChatGPT", "Jira", "Putty", "Excel", "VBA", "XML", "Python", "JavaScript", "MS Access", "Notepad++", "FileZilla", "GraphQL", "TCP Test Tool", "Vocollect", "FlexSim", "AutoCAD"].map((item) => (
+              <div key={item} className={tagStyles}>{item}</div>
+            ))}
+          </div>
+        </div>
+
+        {/* Key Personal Strengths Section */}
+        <div className="mt-16 mb-12">
+          <h2 className="text-2xl font-semibold text-center mb-6 text-[#568EA3]">Key Personal Strengths</h2>
+          <div className="flex flex-wrap justify-center gap-3">
+            {["Problem Solving", "Critical Thinking", "Handling Ambiguity", "Taking Initiative", "Leadership", "Adding Value", "Time Management", "Multi-tasking", "Communication", "Strong Work Ethic"].map((item) => (
+              <div key={item} className={tagStyles}>{item}</div>
+            ))}
+          </div>
+        </div>
+
+        {/* Contact Section */}
+        <div className="mt-12 text-center">
+          <h2 className="text-2xl font-semibold text-[#568EA3]">Let’s Connect</h2>
+          <p className="mt-2 text-lg">Interested in discussing how I can help your team? Let’s talk!</p>
+          <a href="mailto:davispidgeon81@gmail.com" className="mt-4 inline-block px-6 py-2 bg-[#A76D47] text-white rounded-full shadow-md hover:shadow-lg">Contact Me</a>
         </div>
 
 
